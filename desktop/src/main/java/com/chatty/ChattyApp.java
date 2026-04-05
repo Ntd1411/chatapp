942d084220000b54d0b3d07cda8e65bbaa299453
Dung Nguyen <ct070212@actvn.edu.vn>
2026-04-05 13:34:19 +0700

    feat: Set application title and icon on startup

--- desktop/src/main/java/com/chatty/ChattyApp.java
+++ desktop/src/main/java/com/chatty/ChattyApp.java
@@ -3,6 +3,7 @@ package com.chatty;
 import com.chatty.models.User;
 import com.chatty.services.ThemeService;
 import javafx.application.Application;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 import com.chatty.controllers.LoginController;
 import com.chatty.services.AuthService;
@@ -10,6 +11,15 @@ import com.chatty.services.AuthService;
 public class ChattyApp extends Application {
     @Override
     public void start(Stage primaryStage) {
+        // Set tên app và icon
+        primaryStage.setTitle("Kma Chatty");
+        try {
+            Image icon = new Image(getClass().getResource("/logo.png").toExternalForm());
+            primaryStage.getIcons().add(icon);
+        } catch (Exception e) {
+            System.err.println("Icon không tìm thấy: " + e.getMessage());
+        }
+        
         AuthService authService = new AuthService();
         User user = authService.checkAuth();    // lấy người dùng đã từng đăng nhập trước đó
 
@@ -27,9 +37,6 @@ public class ChattyApp extends Application {
             // chuyển hướng ra đăng nhập
             new LoginController().show(primaryStage);
         }
-        
-        // Maximize window (chiếm hết màn hình trừ taskbar)
-        primaryStage.setMaximized(true);
     }
 
     public static void main(String[] args) {
